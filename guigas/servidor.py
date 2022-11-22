9a0d723ce0af3133e0f36ba6a7edbcc3845670dc
breno.sedrez <breno_rocha2011@hotmail.com>
2022-11-22 13:35:53 -0300

    criado servidor e cliente

---
+++ guigas/servidor.py
@@ -0,0 +1,56 @@
+import socket as s
+import threading
+
+HOST = '127.0.0.1'
+PORT = 9999
+
+servidor = s.socket(s.AF_INET, s.SOCK_STREAM)
+servidor.bind((HOST, PORT))
+
+servidor.listen()
+
+clientes = []
+nomes = []
+
+
+def broadcast(mensagem):
+    for cliente in clientes:
+        cliente.send(mensagem)
+
+
+def handle(cliente):
+    while True:
+        try:
+            mensagem = cliente.recv(1024)
+            print(f"{nomes[clientes.index(cliente)]} diz {mensagem}")
+            broadcast(mensagem)
+        except:
+            index = clientes.index(cliente)
+            clientes.remove(cliente)
+            cliente.close()
+            nome = nomes[index]
+            nomes.remove(nome)
+            break
+
+
+def receber():
+    while True:
+        cliente, endereco = servidor.accept()
+        print(f"Conectado com {str(endereco)}")
+
+        cliente.send("NICK".encode('utf-8'))
+        nome = cliente.recv(1024)
+
+        nomes.append(nome)
+        clientes.append(cliente)
+
+        print(f"Nome do cliente: {nome}")
+        broadcast(f"{nome} conectado no servidor!\n".encode('utf-8'))
+        cliente.send("Conectado no servidor".encode('utf-8'))
+
+        thread = threading.Thread(target=handle, args=(cliente,))
+        thread.start()
+
+
+print("Servidor rodando...")
+receber()
